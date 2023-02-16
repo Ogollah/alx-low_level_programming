f61fa21beee1b7a76289ccec2a5d8d67a72b4c2c
Ogollah <kogollah2011@gmail.com>
2023-02-16 09:54:10 +0300

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ * Return: 0 When success
+ */
+int main(void)
+{
+    char str[] = "\"Programming is like building a multilingual puzzle \n";
+    puts(str);
+    return (0);
+}
