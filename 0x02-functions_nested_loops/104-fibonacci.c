d558d0d1244b048b3cd51ff6d435c5f480265752
Ogollah <kogollah2011@gmail.com>
2023-02-21 23:29:52 +0300

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - print the first 98 fib numbers, starting with 1 and 2
+ *
+ *Return:Always 0 (Succes)
+ */
+int main(void)
+{
+	unsigned long int first = 1, sec = 2, next;
+	int i = 0;
+
+	printf("%lu, %lu, ", first, sec);
+	while (i <= 95)
+	{
+		next = first + sec;
+		printf("%lu", next);
+		first = sec;
+		sec = next;
+		if (i < 95)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
