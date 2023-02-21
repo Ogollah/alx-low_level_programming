e9170992ca96182cf5b81e49cbe6067937e50fb5
Ogollah <kogollah2011@gmail.com>
2023-02-21 17:52:18 +0300

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_to_98 - print from n to 98
+ *@n: Value to print from
+ *
+ *Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
+}
