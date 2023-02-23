1ef93be71a2073cd04f0108cc5ce4300873b1858
Ogollah <kogollah2011@gmail.com>
2023-02-23 12:43:06 +0300

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *main - prints Fizz Buzz
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i < 100)
+		{
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
