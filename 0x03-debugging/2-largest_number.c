3fbabce7a3fcfc38c723803c10522ec21268465d
Ogollah <kogollah2011@gmail.com>
2023-02-26 20:16:40 +0300

    Update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,33 +2,28 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- * @a: first integer month
- * @b: second integer day
- * @c: third integer year
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
  * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
 {
-if ((c % 4 == 0 && c % 100 != 0) || c % 400 == 0)
-{
-if (a > 2 && b >= 60)
-{
-b++;
-}
-printf("Day of the year: %d\n", b);
-printf("Remaining days: %d\n", 366 - b);
-}
-else
-{
-if (a == 2 && b == 60)
-{
-printf("Invalid date: %02d/%02d/%04d\n", a, b - 31, c);
-}
-else
-{
-printf("Day of the year: %d\n", b);
-printf("Remaining days: %d\n", 365 - b);
-}
-}
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
 }
