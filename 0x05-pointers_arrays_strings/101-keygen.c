523c65d61bd3202ba65c425d6786ba2ffa3fe0fa
Ogollah <kogollah2011@gmail.com>
2023-02-27 23:36:42 +0300

    Dont hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random valid passwords
+ *
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	int pass[90];
+	int i = 0, sum, n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	while (i < 100)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+		i++;
+	}
+
+	return (0);
+}
