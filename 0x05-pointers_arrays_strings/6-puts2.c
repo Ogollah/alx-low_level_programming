8b6d3f2c9be91cfecaff1c87445df1c27675632d
Ogollah <kogollah2011@gmail.com>
2023-02-27 12:06:14 +0300

    Half the lies they tell about me arent true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *puts2 - Prints every other character of a string starting
+ *        with the first character
+ *@str: String from which characters to be printed
+ *
+ *Return: Void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
