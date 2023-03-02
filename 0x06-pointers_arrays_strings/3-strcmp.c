8499a7b09a5995da943f77674c8a08bcd0c1d349
Ogollah <kogollah2011@gmail.com>
2023-03-02 18:26:16 +0300

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_strcmp - Compares two strings
+ *@s1: String destination
+ *@s2: String source to be copied
+ *
+ *Return: The defference btwn the two strings in bytes
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (*s1 - *s2);
+}
