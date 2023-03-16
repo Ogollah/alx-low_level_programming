8c3a2de5487de310ce7954ddce8932d9131d2b65
Ogollah <kogollah2011@gmail.com>
2023-03-16 13:33:28 +0300

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,6 +14,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len = 0, i, j, con = 0, s2_len = 0;
 	char *mal;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
 	if (s2 == NULL)
 	{
 		s2 = "";
