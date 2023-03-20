8770aa22c78c47981890125c8e06cebad3f8f980
Ogollah <kogollah2011@gmail.com>
2023-03-21 00:02:02 +0300

    Fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,6 +12,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned int name_ln, owner_ln, i;
+	char *name_cpy, *owner_cpy;
 	dog_t *dog_new_name;
 
 	if (name == NULL || owner == NULL || age < 0)
@@ -23,27 +24,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		name_ln++;
 	}
-	dog_new_name->name = malloc((name_ln + 1) * sizeof(char));
-	if (dog_new_name->name == NULL)
+	name_cpy = malloc((name_ln + 1) * sizeof(char));
+	if (name_cpy == NULL)
 	{
 		free(dog_new_name);
 		return (NULL);
 	}
-	for (i = 0; i < name_ln; i++)
-		dog_new_name->name[i] = name[i];
-	dog_new_name->age = age;
+	for (i = 0; name[i] != '\0'; i++)
+		name_cpy[i] = name[i];
 	while (owner[owner_ln])
 	{
 		owner_ln++;
 	}
-	dog_new_name->owner = malloc((owner_ln + 1) * sizeof(char));
+	owner_cpy = malloc((owner_ln + 1) * sizeof(char));
 	if (dog_new_name->owner == NULL)
 	{
 		free(dog_new_name->name);
 		free(dog_new_name);
 		return (NULL);
 	}
-	for (i = 0; i < owner_ln; i++)
-		dog_new_name->owner[i] = owner[i];
+	for (i = 0; owner[i] != '\0'; i++)
+		owner_cpy[i] = owner[i];
+
+	dog_new_name->name = name_cpy;
+	dog_new_name->age = age;
+	dog_new_name->owner = owner_cpy;
 	return (dog_new_name);
 }
