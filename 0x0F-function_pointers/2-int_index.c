190d5e6be99eb2fb007f08ede38ecac18832b7eb
Ogollah <kogollah2011@gmail.com>
2023-03-22 16:49:49 +0300

    To hell with circumstances I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,16 +13,22 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL)
-		return (-1);
-	if (size <= 0)
-		return (-1);
-	if (cmp == NULL)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array == NULL || size <= 0 || cmp == NULL)
 	{
-		if (cmp(array[i]))
-			return (i);
+		return (-1);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+			else
+			{
+				return (-1);
+			}
+		}
 	}
 }
