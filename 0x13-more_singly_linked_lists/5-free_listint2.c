615760f5f7ce72a5a2d4ced18aa13dc50222ad08
Ogollah <kogollah2011@gmail.com>
2023-04-03 19:48:02 +0300

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return;
 
-	while (head)
+	while (*head)
 	{
 		ptr = *head;
 		*head = (*head)->next;
 		free(ptr);
 	}
+	ptr->next = NULL;
 }
