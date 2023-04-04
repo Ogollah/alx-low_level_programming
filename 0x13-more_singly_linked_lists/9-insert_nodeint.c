41285f39ffacf975615e2f16a3dd2ba1e02d62d2
Ogollah <kogollah2011@gmail.com>
2023-04-04 17:36:47 +0300

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,10 +26,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (i = 1; i < idx; i++)
 	{
-		temp = temp->next;
-		if (temp == NULL)
+		if (temp->next == NULL)
 		{
 			return (NULL);
+		}
+		else
+		{
+			temp = temp->next;
 		}
 	}
 	ptr->next = temp->next;
