f9876e55e3f24aca7c206e5e912052d4888c3e8d
Ogollah <kogollah2011@gmail.com>
2023-04-04 22:22:00 +0300

    update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- *get_nodeint_at_index - Search the nth node of a listint_t list.
- *@head: Pointer to the start of the list
- *@index: The nth index.
+ *get_nodeint_at_index - Search the @index node of a listint_t list.
+ *@head: Pointer to the start of the list.
+ *@index: The index.
  *
  *Return: nth node, if node does not exit NULL.
  */
