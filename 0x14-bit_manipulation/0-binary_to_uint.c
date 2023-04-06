fbf893da342da5c11e7094c21251a41a87cbbee7
Ogollah <kogollah2011@gmail.com>
2023-04-06 18:27:38 +0300

    Convert string binary to dec

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,37 @@
 #include "main.h"
 
 /**
+ *_atoi - Converts a string to an integer
+ *@s: String to be converted
+ *
+ *Return: Converted integer
+ */
+int _atoi(char *s)
+{
+	int sign;
+	unsigned int num;
+	char *str;
+
+	str = s;
+	num = 0;
+	sign = 1;
+	while (*str != 0 && (*str < '0' || *str > '9'))
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	if (*str != 0)
+	{
+		do {
+			num = num * 10 + (*str - '0');
+			str++;
+		} while (*str >= '0' && *str <= '9');
+	}
+	return (num * sign);
+}
+
+/**
  *binary_to_uint - Converts a binary number to an unsigned int.
  *@b: A pointer to a string of 0 and 1 chars.
  *
@@ -21,7 +52,7 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 		}
 	}
-	bin_num =  atoi(b);
+	bin_num =  _atoi(b);
 
 	temp = bin_num;
 
