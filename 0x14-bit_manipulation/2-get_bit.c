0b006a65a7b81df89102dc08f6d48d46f2e0c6d2
Ogollah <kogollah2011@gmail.com>
2023-04-09 09:16:07 +0300

    Get bit at index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,5 +11,5 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int size = ((sizeof(unsigned long int) * 8) - 1);
 
-	return ((index > size) ? -1 : (n >> index & 1));
+	return ((index >= size) ? -1 : ((n >> index) & 1));
 }
