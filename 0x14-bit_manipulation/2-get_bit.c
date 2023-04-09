1590eba4722fd65e092907e2adce39e4e4882977
Ogollah <kogollah2011@gmail.com>
2023-04-09 09:14:57 +0300

    Get bit at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *get_bit - Get bit at a given position.
+ *@n: Number to conver to binary.
+ *@index: Byte index to return
+ *
+ *Return: Byte value else -1.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int size = ((sizeof(unsigned long int) * 8) - 1);
+
+	return ((index > size) ? -1 : (n >> index & 1));
+}
