13a3f08a986dd349e00699a1160b8a776400e745
Ogollah <kogollah2011@gmail.com>
2023-04-10 23:35:28 +0300

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,8 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_desc = open(filename, S_IWUSR | S_IRUSR | O_WRONLY |
-			 O_CREAT | O_TRUNC);
+	file_desc = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (file_desc == -1)
 		return (-1);
