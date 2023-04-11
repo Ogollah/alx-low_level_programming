c3ae48cb9d84fddb402ea8cf7addad36f3eec242
Ogollah <kogollah2011@gmail.com>
2023-04-11 15:07:20 +0300

    Update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,23 +15,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	file = open(filename, O_RDONLY);
-
-	if (file == -1)
-		return (0);
-
 	buff = malloc(sizeof(char) * letters);
 
 	if (buff == NULL)
 		return (0);
+
+	file = open(filename, O_RDONLY);
 
 	num_read = read(file, buff, letters);
 
 	num_write = write(STDOUT_FILENO, buff, num_read);
 
 
-	if (num_write == -1 || num_write == -1)
+	if (num_write == -1 || num_write == -1 || file == -1)
+		free(buff);
 		return (0);
 
+	free(buff);
+	close(file);
 	return (num_write);
 }
