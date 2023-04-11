fc357d9cda76af72a71083913b325ca20a0dc0ea
Ogollah <kogollah2011@gmail.com>
2023-04-11 17:50:59 +0300

    Update cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,53 +13,13 @@ void print_error(char *message, char *arg)
 }
 
 /**
- *main -Copy text to another file.
- *@argc: arg count.
- *@argv: arg array.
- *
- *Return: 0.
+ *print_exit_code - Print error and exit code.
+ *@file_desc_from: value 1 to compare.
+ *@file_desc_to: Value 2 compare.
+ *Return: Void
  */
-int main(int argc, char *argv[])
+void print_exit_code(int file_desc_from, int file_desc_to)
 {
-	ssize_t num_read;
-	char *file_from = argv[1], *file_to = argv[2], buffer[BUFFER_SIZE];
-
-	if (argc != 3)
-	{
-		print_error("Usage: cp file_from file_to\n", NULL);
-		exit(97);
-	}
-
-	int file_desc_from = open(file_from, O_RDONLY);
-
-	if (file_desc_from == -1)
-	{
-		print_error("Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
-	int file_desc_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR |
-				S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (file_desc_to == -1)
-	{
-		print_error("Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-
-	while ((num_read = read(file_desc_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		if (write(file_desc_to, buffer, num_read) == -1)
-		{
-			print_error("Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
-	}
-	if (num_read == -1)
-	{
-		print_error("Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
 	if (close(file_desc_from) == -1)
 	{
 		print_error("Error: Can't close fd %d\n", file_desc_from);
@@ -70,5 +30,67 @@ int main(int argc, char *argv[])
 		print_error("Error: Can't close fd %d\n", file_desc_to);
 		exit(100);
 	}
+}
+
+/**
+ *print_arg_error - Print error if arg is not 3.
+ *@arg: No of arg
+ *
+ *Return: Void
+ */
+void print_arg_error(int arg)
+{
+	if (arg != 3)
+	{
+		print_error("Usage: cp file_from file_to\n", NULL);
+		exit(97);
+	}
+}
+
+/**
+ *main -Copy text to another file.
+ *@argc: arg count.
+ *@argv: arg array.
+ *
+ *Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	ssize_t num_r, df, dt, p = (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	char *file_from = argv[1], *file_to = argv[2], buffer[BUFFER_SIZE];
+
+	print_arg_error(argc);
+
+	df = open(file_from, O_RDONLY);
+
+	if (df == -1)
+	{
+		print_error("Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	dt = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, p);
+
+	if (dt == -1)
+	{
+		print_error("Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	while ((num_r = read(df, buffer, BUFFER_SIZE)) > 0)
+	{
+		if (write(dt, buffer, num_r) == -1)
+		{
+			print_error("Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+	if (num_r == -1)
+	{
+		print_error("Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	print_exit_code(df, dt);
 	return (0);
 }
