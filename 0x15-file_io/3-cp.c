68c53e1a1ff737214746bae60b9f365db67753aa
Ogollah <kogollah2011@gmail.com>
2023-04-12 10:59:28 +0300

    Update cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,26 +13,6 @@ void print_error(char *message, char *arg)
 }
 
 /**
- *print_exit_code - Print error and exit code.
- *@file_desc_from: value 1 to compare.
- *@file_desc_to: Value 2 compare.
- *Return: Void
- */
-void print_exit_code(int file_desc_from, int file_desc_to)
-{
-	if (close(file_desc_from) == -1)
-	{
-		print_error("Error: Can't close fd %d\n", file_desc_from);
-		exit(100);
-	}
-	if (close(file_desc_to) == -1)
-	{
-		print_error("Error: Can't close fd %d\n", file_desc_to);
-		exit(100);
-	}
-}
-
-/**
  *print_arg_error - Print error if arg is not 3.
  *@arg: No of arg
  *
@@ -56,41 +36,39 @@ void print_arg_error(int arg)
  */
 int main(int argc, char *argv[])
 {
-	int num_r, df, dt, p = (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	char *file_from = argv[1], *file_to = argv[2], buffer[BUFFER_SIZE];
+	int fd_r, fd_w, i, j, k;
+	char buf[BUFSIZ];
 
 	print_arg_error(argc);
 
-	df = open(file_from, O_RDONLY);
-
-	if (df == -1)
+	fd_r = open(argv[1], O_RDONLY);
+	if (fd_r == -1)
 	{
-		print_error("Error: Can't read from file %s\n", file_from);
+		print_error("Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	dt = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, p);
-
-	if (dt == -1)
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((i = read(fd_r, buf, BUFSIZ)) > 0)
 	{
-		print_error("Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-
-	while ((num_r = read(df, buffer, BUFFER_SIZE)) > 0)
-	{
-		if (write(dt, buffer, num_r) == -1)
+		if (fd_w == -1 || write(fd_w, buf, i) == -1)
 		{
-			print_error("Error: Can't write to %s\n", file_to);
+			print_error("Error: Can't write to %s\n", argv[2]);
+			close(fd_r);
 			exit(99);
 		}
 	}
-	if (num_r == -1)
+
+	if (i == -1)
 	{
-		print_error("Error: Can't read from file %s\n", file_from);
+		print_error("Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	print_exit_code(df, dt);
+	j = close(fd_r);
+	k = close(fd_w);
+	if (j == -1 || k == -1)
+	{
+		print_error("Error: Can't close fd %d\n", fd_r);
+		exit(100);
+	}
 	return (0);
 }
