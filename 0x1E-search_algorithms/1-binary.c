2a4ebcbb93558365867968068c2b9fce87319848
Ogollah <kogollah2011@gmail.com>
2023-10-10 22:05:51 +0300

    Tasks 1-6

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,35 +9,31 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
+	int low = 0, mid, high, index;
+
 	if (size == 0 || array == NULL)
 		return (-1);
 
-	int low = 0;
-	int high = size - 1;
+	high = size - 1;
 
 	while (low <= high)
 	{
-		int middle = (low + high) / 2;
-
 		printf("Searching in array: ");
-		for (size_t index = low; index <= high; index++)
-		{
+		for (index = low; index <= high; index++)
 			printf(index < high ? "%d, " : "%d\n", array[index]);
-		}
-
-		if (array[middle] == value)
+		mid = (high + low) / 2;
+		if (array[mid] < value)
 		{
-			return (middle);
+			low = mid + 1;
 		}
-		else if (array[middle] < value)
+		else if (array[mid] > value)
 		{
-			low = middle + 1;
+			high = mid - 1;
 		}
 		else
 		{
-			high = middle - 1;
+			return (mid);
 		}
 	}
-
 	return (-1);
 }
